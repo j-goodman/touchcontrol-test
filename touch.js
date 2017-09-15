2ec77d919813fa57a542c4fca9f9124660ee5b74
John Goodman <john.goodman@fluentcity.com>
2017-09-15 15:58:40 -0400

    Implement touchcontrols.

--- touch.js
+++ touch.js
@@ -1,17 +1,22 @@
+var touchLog = {};
+
 window.onload = function () {
   var rect;
   rect = document.getElementById('rectangle');
-  console.log(rect);
   rect.addEventListener("touchstart", touchStart, false);
   rect.addEventListener("touchend", touchEnd, false);
 }
 
 function touchStart (event) {
-  document.getElementById('rectangle').style.background = '#00f';
-  document.getElementById('paragraph').innerText = JSON.stringify(event);
+  touchLog.startX = event.changedTouches[0].screenX;
 }
 
 function touchEnd (event) {
-  document.getElementById('rectangle').style.background = '#f00';
-  document.getElementById('paragraph').innerText = JSON.stringify(event);
+  touchLog.endX = event.changedTouches[0].screenX;
+  console.log(touchLog.startX, touchLog.endX)
+  if (touchLog.endX < touchLog.startX) {
+    document.getElementById('rectangle').style.background = '#00f';
+  } else if (touchLog.endX > touchLog.startX){
+    document.getElementById('rectangle').style.background = '#f00';
+  }
 }
